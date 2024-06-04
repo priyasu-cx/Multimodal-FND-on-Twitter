6db304eb164a861b8818de760ea3ce7f45d88216
priyasu-cx <priyasuguin.stu@gmail.com>
2024-06-05 02:02:29 +0530

    changes - debug

--- Prototype/functions/classify_fnd.py
+++ Prototype/functions/classify_fnd.py
@@ -3,6 +3,7 @@ from .checkSattire import *
 import streamlit as st
 import os
 
+
 """
 This function calculates the final result that'll come out of the semantics classifier,
 as one of: ["misinformation", "disinformation", "spam", "satire"]
@@ -13,7 +14,11 @@ def getSemantics(tweet_body, bot_score, phishingPresent):
     print("Tweet:")
     print(tweet_body + "\n")
 
-    os.write("Tweet: ", tweet_body + "Bot Score: ", bot_score, "Phishing Present: ", phishingPresent + "\n")
+    print("Tweet: ", tweet_body + "Bot Score: ", bot_score, "Phishing Present: ", phishingPresent + "\n")
+
+    os.write(1, f"Tweet: {tweet_body}\n".encode()) 
+    os.write(1, f"Bot Score: {bot_score}\n".encode())
+    os.write(1, f"Phishing Present: {phishingPresent}\n".encode())
 
     # Possible results from this classifier
     results = ["misinformation", "disinformation", "spam", "satire"]
@@ -27,20 +32,24 @@ def getSemantics(tweet_body, bot_score, phishingPresent):
 
     # Final Result will be disinformation if user is a bot
     if isBot:
-        os.write("User is a bot, who has shared false news")
+        print("User is a bot, who has shared false news")
+        # print to console
+        os.write(1, "User is a bot, who has shared false news\n".encode())
         final_result = results[1]
         print(final_result)
         return final_result
     else:
         if phishingPresent:
-            os.write("Phishing URL detected. User is not a bot. Disinformation.")
+            print("Phishing URL detected. User is not a bot. Disinformation.")
+            os.write(1, "Phishing URL detected. User is not a bot. Disinformation.\n".encode())
             final_result = results[0]
             print(final_result)
             return final_result
 
-    os.write(
+    print(
         "User is not a bot. Not disinformation.\nChecking for type of misinformation...\n"
     )
+    os.write(1, "User is not a bot. Not disinformation.\nChecking for type of misinformation...\n".encode())
 
     # Processing for spam
     isSpam = checkSpam(tweet_body, phishingPresent)
@@ -51,17 +60,21 @@ def getSemantics(tweet_body, bot_score, phishingPresent):
     # If either one of them is true(1)     => that will the type of misinfo
     # If both of them are true(1)/false(0) => we just say that it's a misinformation
     if (isSpam == 1 and isSatire == 1) or (isSpam == 0 and isSatire == 0):
-        os.write("Couldn't cleary detect whether spam or satire")
+        print("Couldn't cleary detect whether spam or satire")
+        os.write(1, "Couldn't cleary detect whether spam or satire\n".encode())
         final_result = results[0]
 
     elif isSpam == 1:
-        os.write("Tweet could be a potential spam!!".upper())
+        print("Tweet could be a potential spam!!".upper())
+        os.write(1, "Tweet could be a potential spam!!\n".encode())
         final_result = results[2]
     else:
-        os.write("Tweet could be a potential satire!!".upper())
+        print("Tweet could be a potential satire!!".upper())
+        os.write(1, "Tweet could be a potential satire!!\n".encode())
         final_result = results[3]
 
-    os.write("Final outcome: " + final_result)
+    print("Final outcome: " + final_result)
+    os.write(1, f"Final outcome: {final_result}\n".encode())
 
     return final_result
 # Testing the function
